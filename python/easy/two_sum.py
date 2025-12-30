660dd9ddddc519656e8fb2f9d901449e3edc7c86
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2025-12-30 12:46:11 +0500

    refactor: improve solution of two sum

--- python/easy/two_sum.py
+++ python/easy/two_sum.py
@@ -30,28 +30,26 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i, n in enumerate(nums):
-            x = target - n
-            if x == nums[i + 1]:
-                return [i, i + 1]
-            if x in nums[i + 1 :]:
-                return [i, nums[i + 1 :].index(x) + i + 1]
-        return [0]
+        hm = {nums[0]: 0}
+        for i in range(1, len(nums)):
+            missing = target - nums[i]
+            if missing in hm:
+                return [i, hm[missing]]
+            hm[nums[i]] = i
 
 
 solution = Solution()
 
 # Testcase 1
 res = solution.twoSum([2, 7, 11, 15], 9)
-print(res, end=" ")
-print(res == [0, 1])  # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+print(
+    res, set(res) == {0, 1}
+)  # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 # Testcase 2
 res = solution.twoSum([3, 2, 4], 6)
-print(res, end=" ")
-print(res == [1, 2])
+print(res, set(res) == {1, 2})
 
 # Testcase 3
 res = solution.twoSum([3, 3], 6)
-print(res, end=" ")
-print(res == [0, 1])
+print(res, set(res) == {0, 1})
