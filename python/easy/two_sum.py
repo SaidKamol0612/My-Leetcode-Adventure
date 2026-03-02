cc32cebcec015d215e82cb03ed71210034674ec0
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2026-03-02 10:55:34 +0500

    improve solution of two sum

--- python/easy/two_sum.py
+++ python/easy/two_sum.py
@@ -1,41 +1,16 @@
-class Solution(object):
+class Solution:
     """
-        # Two Sum
-    Difficulty: EASY.
-
     [View this problem on Leetcode](https://leetcode.com/problems/two-sum/)
-
-    ## Description
-    Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
-
-    You may assume that each input would have **exactly one solution**, and you amy not use the same element twice.
-
-    You can return the answer in any order.
-
-    ## Examples
-
-    **Input:** nums = [2, 7, 11, 15], target = 9
-    **Output:** [0, 1]
-
-    **Input:** nums = [3, 2, 4], target = 6
-    **Output:** [1, 2]
-
-    **Input:** nums [3, 3], target = 6
-    **Output:** [0, 1]
     """
 
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         hm = {nums[0]: 0}
         for i in range(1, len(nums)):
             missing = target - nums[i]
             if missing in hm:
                 return [i, hm[missing]]
             hm[nums[i]] = i
+        return [0, 1]
 
 
 solution = Solution()
