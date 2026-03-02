950a216871c1382ef23ffa7822f327b696bf80fc
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2026-03-02 10:55:09 +0500

    improve solution of valid parentheses

--- python/easy/valid_parentheses.py
+++ python/easy/valid_parentheses.py
@@ -1,64 +1,28 @@
-class Solution(object):
+class Solution:
     """
-        # Valid Parentheses
-
-    Difficulty: EASY.
-
     [View this problem on Leetcode](https://leetcode.com/problems/valid-parentheses/)
-
-    ## Description
-
-    Given an string `s` containing just the characters `'('`, `')'`, `'{'`, `'}'`, `'['`, and `']'`, determine if the input string is valid.
-
-    An input string is valid if:
-
-    1. Open brackets must be closed by the same type of brackets.
-    2. Open brackets must be closed in the correct order.
-    3. Every close bracket has a corresponding open bracket of the same type.
-
-    ## Examples
-
-    **Input:** s = "()"
-    **Output:** true
-
-    **Input:** s = "()[]{}"
-    **Output:** true
-
-    **Input:** s = "(]"
-    **Output:** false
-
-    **Input:** s = "([])"
-    **Output:** true
-
-    **Input:** s = "([)]"
-    **Output:** false
     """
 
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+    def isValid(self, s: str) -> bool:
+        # if len(s) < 2 or len(s) % 2 != 0:
+        #     return False
 
-        if len(s) < 2 or len(s) % 2 != 0:
-            return False
+        parentheses = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }
 
-        t = []
+        opened: list[str] = []
         for c in s:
-            if c in "([{":
-                t.append(c)
+            if c in parentheses:
+                opened.append(c)
             else:
-                if not t:
+                if len(opened) == 0 or parentheses[opened[-1]] != c:
                     return False
+                opened.pop()
 
-                top = t.pop()
-                if c == ")" and top != "(":
-                    return False
-                elif c == "]" and top != "[":
-                    return False
-                elif c == "}" and top != "{":
-                    return False
-        return not t
+        return len(opened) == 0
 
 
 solution = Solution()
